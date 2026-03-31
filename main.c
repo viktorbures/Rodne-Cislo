82a95f31971248cc6c142c2e8d7eac4edd510b48
Viktor Bureš <89810093+viktorbures@users.noreply.github.com>
2026-03-31 10:56:51 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -10,7 +10,6 @@
 #define MAX_MONTH 12
 
 #define MIN_DAY 1
-#define MAX_DAY 31
 
 #define MIN_POHLAVI 0
 #define MAX_POHLAVI 1
@@ -44,57 +43,92 @@ int zadaneCislo(int min, int max, const char *text) {
         } else if (hodnota > max || hodnota < min) {
             printf("Zadane cislo neni v intervalu od %d do %d. \n", min, max);
         }
-
     } while (hodnota > max || hodnota < min || !characters);
 
     return hodnota;
 }
 
-void rodneCislo(int rok, int mesic, int den, int pohlavi, int trojcisli, int posledniCislo, char * rc) {
-    int r, m, d, p, t, pc;
+int prestupnyRok(int rok) {
+    return (rok % 4 == 0 && rok % 100 != 0) || rok % 400 == 0;
+}
 
-    r = rok % 100;
-
-    if(pohlavi == 0) {
-        m = mesic + 50;
+int dniVMesici(int rok, int mesic) {
+    switch (mesic) {
+        case 1:
+            return 31;
+        case 2:
+            return prestupnyRok(rok) ? 29 : 28;
+        case 3:
+            return 31;
+        case 4:
+            return 30;
+        case 5:
+            return 31;
+        case 6:
+            return 30;
+        case 7:
+            return 31;
+        case 8:
+            return 31;
+        case 9:
+            return 30;
+        case 10:
+            return 31;
+        case 11:
+            return 30;
+        case 12:
+            return 31;
+        default:
+            return 0;
     }
-    else {
+}
+
+int posledniCislo(long long int zaklad) {
+    int zbytek = (zaklad * 10) % 11;
+
+    return (11 - zbytek) % 11;
+}
+
+void rodneCislo(int rok, int mesic, int den, int pohlavi, int trojcisli, int posledniCislo, char *rc) {
+    int r = rok % 100;
+    int m;
+
+    if (pohlavi == 0) {
+        m = mesic + 50;
+    } else {
         m = mesic;
     }
 
-    d = den;
-    t = trojcisli;
-    pc = posledniCislo;
-
-    sprintf(rc, "%02d%02d%02d/%03d%d", r, m, d, t, pc);
+    sprintf(rc, "%02d%02d%02d/%03d%d", r, m, den, trojcisli, posledniCislo);
 }
 
 int main(void) {
     srand((unsigned) time(NULL));
     char again;
     char rc[20];
+    char zakladStr[20];
 
     do {
         int rok = zadaneCislo(MIN_YEAR, MAX_YEAR, "Zadej rok");
         int mesic = zadaneCislo(MIN_MONTH, MAX_MONTH, "Zadej mesic");
-        int den = zadaneCislo(MIN_DAY, MAX_DAY, "Zadej den");
+
+        int maxDen = dniVMesici(rok, mesic);
+        int den = zadaneCislo(MIN_DAY, maxDen, "Zadej den");
+
         int pohlavi = zadaneCislo(MIN_POHLAVI, MAX_POHLAVI, "Zadej pohlavi (0 - zena, 1 - muz)");
         int trojcisli = zadaneCislo(MIN_TROJCISLI, MAX_TROJCISLI, "Zadej trojcisli");
-        int posledniCislo = rand() % 10;
 
-        rodneCislo(rok, mesic, den, pohlavi, trojcisli, posledniCislo, rc);
+
+        sprintf(zakladStr, "%02d%02d%02d%03d", rok, mesic, den, trojcisli);
+        long long int zaklad = atoll(zakladStr);
+
+        int posledniNum = posledniCislo(zaklad);
+        rodneCislo(rok, mesic, den, pohlavi, trojcisli, posledniNum, rc);
         printf("Rodne cislo je: %s \n", rc);
 
         printf("Pro opakovani zadani rodneho cisla stiskni Enter: \n");
         scanf("%c", &again);
-    } while(again == '\n');
-
-    /* DODELAT:
-        - Prestupny rok
-        - Kazdy mesic ma jiny pocet dnu
-        - Neco s delenim %11 idk
-    */
-
+    } while (again == '\n');
 
     return 0;
 }
